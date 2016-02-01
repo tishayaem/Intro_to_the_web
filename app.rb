dd8dce7427320cb076a70e00efd8d6a41aa5bac0
Constantin <kostiantynkalinin@gmail.com>
2016-02-01 16:21:29 +0000

    Challenge 12: Params

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/news' do
 'test3'
 end
 
-get '/cat' do
+get '/random-cat' do
 @name_sample = ["Amigo", "Oscar", "Viking"].sample
+erb(:index)
+end
+
+get '/named-cat' do
+p params
+@name_sample = params[:name]
 erb(:index)
 end
