c193a35487f954a0ad083c1f59df14056c5b0bfe
Constantin <kostiantynkalinin@gmail.com>
2016-02-01 16:00:03 +0000

    Challenge 11: create instance variable

--- app.rb
+++ app.rb
@@ -14,7 +14,6 @@ get '/news' do
 end
 
 get '/cat' do
- "<a> <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
- </a>"
-
+@name_sample = ["Amigo", "Oscar", "Viking"].sample
+erb(:index)
 end
