066ff0d9cca00bab7ddb579d8db27ef73555db8b
Constantin <kostiantynkalinin@gmail.com>
2016-02-02 10:33:47 +0000

    Challenge 14

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+
 set :session_secret, 'super secret'
 
 get '/' do
@@ -15,11 +16,15 @@ end
 
 get '/random-cat' do
 @name = ["Amigo", "Oscar", "Viking"].sample
-erb(:index)
+erb :index
 end
 
-get '/named-cat' do
+post '/named-cat' do
 p params
 @name = params[:name]
-erb(:index)
+erb :index
+end
+
+get '/cat-form' do
+erb (:cat_form)
 end
